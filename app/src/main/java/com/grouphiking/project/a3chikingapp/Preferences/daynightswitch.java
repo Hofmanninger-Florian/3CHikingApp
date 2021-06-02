519f9b35aa78bd2d71341407d8483a04178e16a4
Lucas Kronlachner <58300254+lucaskroni@users.noreply.github.com>
2021-06-02 12:17:26 +0200

    Master Merge PLS Daddy (#10)
    * Test Commit 2
    
    * Test Commit 2
    
    * Colors Update and others
    
    Co-authored-by: Lucas Kronlachner <77855627+lucaskronlachner@users.noreply.github.com>

---
+++ app/src/main/java/com/grouphiking/project/a3chikingapp/Preferences/daynightswitch.java
@@ -0,0 +1,30 @@
+package com.grouphiking.project.a3chikingapp.Preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+
+import com.grouphiking.project.a3chikingapp.R;
+
+public class daynightswitch extends Preference {
+
+
+    public daynightswitch(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setWidgetLayoutResource(R.layout.perf_daynightswitch);
+    }
+
+    public daynightswitch(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        //Set Buttonfunktions
+    }
+
+
+}
