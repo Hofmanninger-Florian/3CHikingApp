52cfd487e245d6ecac8f2378b88fc8b992d24ad1
Lucas Kronlachner <77855627+lucaskronlachner@users.noreply.github.com>
2021-06-09 12:35:56 +0200

    Commit because of Stress

--- app/src/main/java/com/grouphiking/project/a3chikingapp/Preferences/LanguageSpinner.java
+++ app/src/main/java/com/grouphiking/project/a3chikingapp/Preferences/LanguageSpinner.java
@@ -2,8 +2,13 @@ package com.grouphiking.project.a3chikingapp.Preferences;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+
+import com.grouphiking.project.a3chikingapp.R;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,8 +18,15 @@ public class LanguageSpinner extends Preference {
 
     private List<String> languages = new ArrayList<String>();
 
+    private Spinner spinner;
+
     public LanguageSpinner(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        setWidgetLayoutResource(R.layout.pref_languagespinner);
+    }
+
+    public LanguageSpinner(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
     }
 
     @Override
@@ -23,7 +35,19 @@ public class LanguageSpinner extends Preference {
         super.onAttached();
     }
 
-    public LanguageSpinner(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        holder.itemView.setClickable(false);
+        spinner = holder.itemView.findViewById(R.id.prefs_language_spinner);
+        setAdapter();
     }
+
+    private void setAdapter() {
+        if(spinner != null) {
+            LanguageSpinnerAdapter adapter = new LanguageSpinnerAdapter(getContext(), R.layout.spinner_item, languages);
+            spinner.setAdapter(adapter);
+        }
+    }
+
 }
