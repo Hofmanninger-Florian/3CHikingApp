51138c3674046df6f6298d122b95476f946aa250
Lucas Kronlachner <77855627+lucaskronlachner@users.noreply.github.com>
2021-06-09 14:24:00 +0200

    Commit MainActivity.java

--- app/src/main/java/com/grouphiking/project/a3chikingapp/Activitys/LoginActivity.java
+++ app/src/main/java/com/grouphiking/project/a3chikingapp/Activitys/LoginActivity.java
@@ -1,14 +1,18 @@
-package com.grouphiking.project.a3chikingapp;
+package com.grouphiking.project.a3chikingapp.Activitys;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.grouphiking.project.a3chikingapp.Data.User;
+import com.grouphiking.project.a3chikingapp.R;
 
 import java.util.ArrayList;
 
@@ -20,6 +24,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+        getSupportActionBar().hide();
         Button loginButton = (Button)findViewById(R.id.button_login);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,12 +38,14 @@ public class LoginActivity extends AppCompatActivity {
 
     private void checkLogin(){
         System.out.println("in method");
-        EditText editText_username = findViewById(R.id.username_textField);
-        EditText editText_password = findViewById(R.id.password_textField);
-        if(editText_username.getText().equals("") || editText_password.getText().equals("")){
-            Toast.makeText(this, "Invalid Password or Username", Toast.LENGTH_SHORT).show();
+        RelativeLayout layout = (RelativeLayout)findViewById(R.id.login_contextlayout);
+        EditText editText_username = (EditText) findViewById(R.id.username_textField);
+        EditText editText_password = (EditText) findViewById(R.id.password_textField);
+        if(editText_username.getText().toString().equals("") || editText_password.getText().toString().equals("")){
+            Snackbar.make(layout, R.string.login_snackbarLabel, Snackbar.LENGTH_SHORT).show();
         } else {
-            setContentView(R.layout.activity_main);
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
         }
     }
 }
---
+++ app/src/main/java/com/grouphiking/project/a3chikingapp/Activitys/MainActivity.java
@@ -0,0 +1,16 @@
+package com.grouphiking.project.a3chikingapp.Activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.grouphiking.project.a3chikingapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
