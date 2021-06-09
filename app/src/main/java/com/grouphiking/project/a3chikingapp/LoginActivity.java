42881f64212047366c44555eb87c2a8c0b5cb100
Lucas Kronlachner <77855627+lucaskronlachner@users.noreply.github.com>
2021-06-09 13:50:38 +0200

    Commit

--- app/src/main/java/com/grouphiking/project/a3chikingapp/LoginActivity.java
+++ app/src/main/java/com/grouphiking/project/a3chikingapp/LoginActivity.java
@@ -1,33 +1,43 @@
 package com.grouphiking.project.a3chikingapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.grouphiking.project.a3chikingapp.Data.User;
 
 import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
 
-    ArrayList<User> userList = new ArrayList<User>();
+    ArrayList<User> LogeduserList = new ArrayList<User>();
+
+    private EditText username;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
-        Button loginButton = (Button)findViewById(R.id.button_login);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        getActionBar().hide();
+        getSupportActionBar().hide();
+        password = (EditText)findViewById(R.id.password_textField);
+        username = (EditText)findViewById(R.id.username_textField);
+        ((Button)findViewById(R.id.button_login)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("Hallo");
+
             }
         });
 
     }
 
-    public void onClickListenerSettings1(View view) {
-    }
+
+
+
 }
