5df59e49f767f3b6f2c2120f5166a163e999c73f
elpanic <leonhard.panic@outlook.de>
2021-06-09 14:25:46 +0200

    checkRegister, LoginActivity.java

--- app/src/main/java/com/grouphiking/project/a3chikingapp/LoginActivity.java
+++ app/src/main/java/com/grouphiking/project/a3chikingapp/LoginActivity.java
@@ -2,10 +2,12 @@ package com.grouphiking.project.a3chikingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.grouphiking.project.a3chikingapp.Data.User;
@@ -20,6 +22,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+        TextView textView = (TextView)findViewById(R.id.no_account);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("aaa");
+                checkRegister();
+            }
+        });
         Button loginButton = (Button)findViewById(R.id.button_login);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,13 +42,31 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void checkLogin(){
-        System.out.println("in method");
-        EditText editText_username = findViewById(R.id.username_textField);
-        EditText editText_password = findViewById(R.id.password_textField);
-        if(editText_username.getText().equals("") || editText_password.getText().equals("")){
+        System.out.println("in checkLogin");
+        EditText editText_username = (EditText)findViewById(R.id.username_textField);
+        EditText editText_password = (EditText)findViewById(R.id.password_textField);
+
+        if(editText_username.getText().toString().equals("") || editText_password.getText().toString().equals("")){
             Toast.makeText(this, "Invalid Password or Username", Toast.LENGTH_SHORT).show();
         } else {
-            setContentView(R.layout.activity_main);
+            boolean b = false;
+            for(User u : userList){
+                if(editText_username.getText().toString().equals(u.getUsername()) && editText_password.getText().toString().equals(u.getPassword())){
+                    b = true;
+                }
+            }
+            if(b){
+                //Intent intent = new Intent(this, MainActivity.class);
+            }
+
+
+
         }
+    }
+    public void checkRegister(){
+        //add
+        /*String username = editText_username.getText().toString();
+        String password = editText_password.getText().toString();
+        userList.add(new User(username, password));*/
     }
 }
