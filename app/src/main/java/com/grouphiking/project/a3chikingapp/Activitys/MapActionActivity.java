2d9f74d5dfd7b2ac7368b37efec66e46852914b1
Florian Hofmanninger <florian.hofmanninger@gmail.com>
2021-06-15 18:43:22 +0200

    Mapbox added dependencies, token, etc...

--- app/src/main/java/com/grouphiking/project/a3chikingapp/Activitys/MapActionActivity.java
+++ app/src/main/java/com/grouphiking/project/a3chikingapp/Activitys/MapActionActivity.java
@@ -5,12 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.grouphiking.project.a3chikingapp.R;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
 
 public class MapActionActivity extends AppCompatActivity {
+
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
         setContentView(R.layout.activity_map);
+
     }
 }
