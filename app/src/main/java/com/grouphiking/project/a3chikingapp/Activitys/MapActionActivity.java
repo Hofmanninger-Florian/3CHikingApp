8382cf079873542d333b7c4d8428b0d7307b69f3
Florian Hofmanninger <florian.hofmanninger@gmail.com>
2021-06-15 19:02:30 +0200

    Mapbox added Basic Mapbox Method

--- app/src/main/java/com/grouphiking/project/a3chikingapp/Activitys/MapActionActivity.java
+++ app/src/main/java/com/grouphiking/project/a3chikingapp/Activitys/MapActionActivity.java
@@ -1,5 +1,6 @@
 package com.grouphiking.project.a3chikingapp.Activitys;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,6 +8,9 @@ import android.os.Bundle;
 import com.grouphiking.project.a3chikingapp.R;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
 
 public class MapActionActivity extends AppCompatActivity {
 
@@ -19,5 +23,17 @@ public class MapActionActivity extends AppCompatActivity {
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
         setContentView(R.layout.activity_map);
 
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                mapboxMap.setStyle(Style.OUTDOORS, new Style.OnStyleLoaded(){
+                    public void onStyleLoaded(@NonNull Style style){
+
+                    }
+                });
+            }
+        });
     }
 }
